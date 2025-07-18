1cb3295c91821a56141182b6a473e085937c7745
Nazmul64 <nazmulhossain3691215@gmail.com>
2025-07-18 14:44:41 +0600

    data type done

--- datatype.js
+++ datatype.js
@@ -1,1 +1,8 @@
-fsdfsfsf
+let name ="My Name is nazmul hossain"
+console.log(name );
+
+let age =25;
+console.log(age);
+
+let getmessages =`Welcome to bangladesh,${name}`
+console.log(getmessages);
---
+++ datatype2.js
@@ -0,0 +1,28 @@
+let name ="Nazmul Hossain";
+console.log(name);
+
+// data type 4 
+
+/*
+ 1.string
+ 2 integer
+ 4 null 
+ 5 undefiend
+*/
+// integer
+let  age =10;
+console.log(age);
+
+
+// null
+
+let number;
+
+
+ number =20;
+ console.log(number);
+
+ // undefiend
+
+ let ageg
+ console.log(ageg);
