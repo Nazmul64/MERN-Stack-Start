d17fab6150cfa6eff792953f980583de15f7f0fe
Nazmul64 <nazmulhossain3691215@gmail.com>
2025-07-23 12:25:19 +0600

    Arithmetic Operators

---
+++ Arithmetic_operators.js
@@ -0,0 +1,44 @@
+// 1. Addition Operator
+
+let x=5
+let y =10
+let z =x+y;
+console.log('ther number of sum=',z);
+
+// Subtraction Operator
+let m=5
+let s =10
+let j =m-s;
+console.log('ther number of sum=',j);
+// 2. Multiplication Operator
+let a=5
+let b =10
+let c =a*b;
+console.log('ther number of sum=',c);
+
+ 
+// Exponentiation Operator
+
+let k =5;
+
+let t =10;
+
+const exponentiation=k**10;
+console.log(exponentiation)
+
+// Division Operator
+
+let number1=10
+let number2 =5
+let total =number1/number2;
+console.log('ther number Division Operator =',total);
+
+// Modulus  Operator  (Division Remainder)
+
+let number3=10
+let number4 =5
+let totalnumber =number3%number4;
+console.log('ther number Modulus Operator =',totalnumber);
+
+
+
