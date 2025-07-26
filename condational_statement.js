916a0a67200175b9195f99aad42f44c4e72198f3
Nazmul64 <nazmulhossain3691215@gmail.com>
2025-07-27 00:50:44 +0600

    condational statement done

---
+++ condational_statement.js
@@ -0,0 +1,9 @@
+let grade =90;
+
+if(grade>=90){
+    console.log('Your Grade number is A+');
+}else if(grade>=80){
+    console.log('Your Grade number is A');
+}else{
+     console.log('Your Grade number is F');
+}
