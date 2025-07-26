9075a6f7eebcca1a6424e2028f0839d9b72ce2c4
Nazmul64 <nazmulhossain3691215@gmail.com>
2025-07-27 00:20:33 +0600

    boolean complate true or false

---
+++ boolean.js
@@ -0,0 +1,11 @@
+let javascriptIsfun =true;
+let mathisheart =false;
+console.log(javascriptIsfun);
+
+
+
+let number1 =50;
+let number2 =100;
+
+console.log(number1 > number2);
+
---
+++ math.js
@@ -0,0 +1,26 @@
+let project1 =20.5;
+let project2=22.5;
+
+const result =Math.ceil(project2);
+console.log(result);
+
+//const result =Math.round(project2);
+//console.log(result);
+
+
+const results =Math.random(0,5);
+console.log(results);
+
+
+
+let number1=25;
+let number2=30;
+let number3=45;
+
+
+const GreetingMaxvalue=Math.max(number1,number2,number3);
+console.log(GreetingMaxvalue);
+
+
+
+
